d71e5b4a686ce7394d35db914f6013b7adbcb560
mandasadeveloper <mandasadeveloper@gmail.com>
2022-06-15 14:13:14 +0530

    third commit

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -27,7 +27,7 @@ export function HomePage() {
             >
               <Stack.Item fill>
                 <TextContainer spacing="loose">
-                  <Heading>Customer Dashboard app</Heading>
+                  <Heading>Customer Dashboard Pro app</Heading>
                   <p>
                     Your app is ready to explore! It contains everything you
                     need to get started including the{" "}
